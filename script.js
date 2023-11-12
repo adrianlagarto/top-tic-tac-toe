df091cd73f41b8dc6bc47208621461ed159b3c81
Adrian Lagarto <adrianlagartomc@gmail.com>
2023-11-12 14:40:26 -0800

    make divs for gamboard

--- script.js
+++ script.js
@@ -4,27 +4,74 @@ const GameBoard = (() => {
 
   const render = () => {
     let boardHTML = "";
-    gameboard.forEach((square,index) => {
-      boardHTML += `<div class="square" id="square-${index}>${square}</div>`
+    gameboard.forEach((square, index) => {
+      boardHTML +=`<div class='square' id='square-${index}'>${square}</div>`;
+    })
+    document.querySelector('#gameboard').innerHTML = boardHTML;
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+      square.addEventListener("click", Game.handleClick(event))
     })
   }
-  document.querySelector('gameboard').innerHTML = boardHTML;
-
   return {
-    render
+    render,
   }
 })();
 
+const createPlayer = (name, mark) => {
+  return {
+    name,
+    mark
+  }
+}
 
+const Game = (() => {
+  let players = [];
+  let currentPlayerIndex;
+  let gameOver;
 
-
-
-
+  const start = () => {
+    players = [
+      createPlayer(document.querySelector('#player1').value, 'X'),
+      createPlayer(document.querySelector('#player2').value, 'O')
+    ]
+    currentPlayerIndex = 0;
+    gameOver = false;
+    GameBoard.render();
+  }
+  const handleClick = (event) => {
+    console.log(event)
+  }
+  return {
+    start,
+    handleClick
+  }
+})();
 
 const startButton = document.querySelector('#start-btn');
 startButton.addEventListener("click", () => {
-  Game.start
+  console.log('start')
+  Game.start();
+  console.log('finish')
+})
 
+
+const playerChoosingSquare = (name, mark, index) => {
+  //checks if the square container has "X" "O"
+  //only fill up the empty square
+  //update the divs when player pick the divs
+}
+
+/*
+const playerChoose = document.querySelector('.square');
+  playerChoose.addEventListener("click", () => {
+    playerChoosingSquare(mark, index);
+    GameBoard.render();
+    //gameboard.render at the end to update
+  })();
+*/
+
+// button on divs to check for 
 /*
 winning combinations to check every move
 
